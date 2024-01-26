0213137ee9c176ef13fb7835b3766e0934a482bd
amira <amira.moadeb@gmail.com>
2024-01-26 20:30:48 +0100

    creation crud user et product

--- server.js
+++ server.js
@@ -37,4 +37,86 @@ catch(error){
     res.send(error);
 
 }
+});
+
+
+app.delete('/deleteuser/:id' ,async(req,res)=>{
+
+try{
+id=req.params.id;
+users=await User.findByIdAndDelete({_id:id});
+res.send(users);
+}
+catch(error){
+res.send(error);
+}
+});
+
+app.put('/updateuser/:id',async(req,res)=>{
+try{
+    id=req.params.id;
+    newdata=req.body;
+    users= await User.findByIdAndUpdate({_id:id}, newdata)
+    res.send(users);
+}
+catch(error){
+res.send(error);
+}
 })
+
+
+//crud products
+app.post('/createproduct',(req,res)=>{
+
+    data=req.body;
+    p=new Product(data)
+    p.save()
+    .then(
+        (productcreated)=>{
+            res.send(productcreated)
+        }
+    )
+    .catch(
+        (error)=>{
+            res.status(401).send("product not created")
+        }
+    )
+    
+    });
+    
+    app.get('/getproduct',async(req,res)=>{
+    try{
+        products=await Product.find();
+        res.send(products);
+    
+    }
+    catch(error){
+        res.send(error);
+    
+    }
+    });
+    
+    
+    app.delete('/deleteproduct/:id' ,async(req,res)=>{
+    
+    try{
+    id=req.params.id;
+    products=await Product.findByIdAndDelete({_id:id});
+    res.send(products);
+    }
+    catch(error){
+    res.send(error);
+    }
+    });
+    
+    app.put('/updateproduct/:id',async(req,res)=>{
+    try{
+        id=req.params.id;
+        newdata=req.body;
+        products= await Product.findByIdAndUpdate({_id:id}, newdata)
+        res.send(products);
+    }
+    catch(error){
+    res.send(error);
+    }
+    })
