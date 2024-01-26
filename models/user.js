4af1bacb967e37daa987e447e199dae490a4c86e
amira <amira.moadeb@gmail.com>
2024-01-26 17:34:47 +0100

    api d'ajout et d'affichage de users

--- models/user.js
+++ models/user.js
@@ -1,10 +1,9 @@
 const mongoose=require('mongoose');
 
-const User=mongoose.model('user',{
+const User=mongoose.model('user', {
 name: {type : String},
 lastname: {type : String},
-email: {type : String},
-password: {type : Number},
+password: {type : Number}
 
 })
 
--- server.js
+++ server.js
@@ -1,14 +1,40 @@
 const express=require('express');
-
-const app=express();
+const User = require('./models/user');
+const Product=require('./models/product');
 require('./config/connect');
 
+const app=express();
+app.use(express.json());
 app.listen(3000,()=>{
     console.log("server work")
 })
-
+//crud user
 app.post('/create',(req,res)=>{
 
+data=req.body;
+usr=new User(data)
+usr.save()
+.then(
+    (usercreated)=>{
+        res.send(usercreated)
+    }
+)
+.catch(
+    (error)=>{
+        res.status(401).send("user not created")
+    }
+)
 
+});
 
+app.get('/getusers',async(req,res)=>{
+try{
+    users=await User.find();
+    res.send(users);
+
+}
+catch(error){
+    res.send(error);
+
+}
 })
